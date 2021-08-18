51054d43978f61e38fe84d408dcbbda7f7deba89
Claudio Armando <claudioelreloco@gmail.com>
2021-08-18 18:06:11 -0300

    juego de adivinar el numero terminado

---
+++ AdivinarElNumero/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AdivinarElNumero
+{
+	class Program
+	{
+		static public Random rand = new Random();
+
+		static void Main(string[] args)
+		{
+			int num = rand.Next(0, 100);
+			int respuesta;
+			do
+			{
+				Console.WriteLine("Elija un numero del 1 al 100: ");
+				respuesta = Convert.ToInt32(Console.ReadLine());
+				if (respuesta < num)
+					Console.WriteLine("El numero elegido es menor al numero aleatorio.");
+				else if (respuesta > num)
+					Console.WriteLine("El numero elegido es mayor al numero aleatorio.");
+				else
+					Console.WriteLine("El numero es el correcto :D.");
+			} while (respuesta != num);
+		}
+	}
+}
